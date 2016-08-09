f107daac131c2615666ec16dd13fc9cd374c725a
Fred Wibaux <fwibaux@gmail.com>
2016-08-09 09:47:09 +0200

    Exercize 2 Longest Word Game : work in progress

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'play/game', to: 'play#game'
+
+  get 'play/score', to: 'play#score'
+
+  get root to: 'play#home'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
